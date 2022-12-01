1186fa330b9e42ad4a402808bf6e1d35f2db13e8
Rafael Ribeiro <101113085+rribeiro-1@users.noreply.github.com>
2022-12-01 16:21:55 +0000

    AOV 2022 Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,31 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.caloriesPerElf().max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.caloriesPerElf().sortedDescending().take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
+}
+
+fun List<String>.caloriesPerElf(): List<Int> {
+    val res = mutableListOf<List<Int>>()
+    var prev = 0
+    this.withIndex()
+        .filter { it.value.isBlank() }
+        .forEach { i ->
+            res.add(this.subList(prev, i.index).map { it.toInt() })
+            prev = i.index + 1
+        }
+    res.add(this.subList(prev, this.size).map { it.toInt() })
+    return res.map { it.sum() }
 }
